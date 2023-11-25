e666889c22b9b58db3762aec12322be0ca159468
sebastian <lukasebastianherrera2004@gmail.com>
2023-11-25 14:26:16 -0300

    PreEntrega1

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -17,17 +17,45 @@ router.post("/", (req, res) => {
 })
 
 router.get("/:id", (req, res) =>{
-    const cartId = req.params.id;
-    const cart = carts.find(cart => cart.id === cartId);
+    const { id } = req.params;
+
+    const cart = carts.find((cart) => cart.id === Number(id));
 
     if (!cart) {
         return res.json({
-            error: "Carrito no encontrado"
+            error: `Error el carrito con id: ${id} no existe`
         });
+    } else {
+        res.send(cart)
     }
-
-    res.json(cart.products);
 });
+// http://localhost:8080/api/carts/ (enviar {} desde body)
+
+router.post("/:cid/product/:pid", (req, res) => {
+    const cartId = req.params.cid;
+    const productId = req.params.pid;
+    const quantity = req.body.quantity || 1;
+    const cart = carts.find(cart => cart.id === cartId);
+    if(!cart){
+        return res.json({
+            error: "carrito no encontrado"
+        })
+    }
+    const productoExistente = cart.products.find(product => product.id === productId);
+
+    if(productoExistente){
+        existingProduct.quantity += quantity;
+    } else {
+        cart.products.push({
+            id: productId,
+            quantity: quantity
+        })  
+    }
+    res.json({
+        status: "Producto agregado al carrito",
+        cartId: cartId
+    });
+})
 
 
 export default router;
